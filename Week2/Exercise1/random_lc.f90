6ae9c940ccb47f2da586a886e38aeffb4b3633a9
Aiace9 <ruggero90@gmail.com>
2015-03-13 10:59:44 +0100

    improve minimum searching, and period finider

--- Week2/Exercise1/random_lc.f90
+++ Week2/Exercise1/random_lc.f90
@@ -5,34 +5,60 @@ program random_lcm
   implicit  none
   !
   !     declaration of variables
-  integer :: i, number, old, seed, x, a,m,c
-  integer :: max = 0, min
-  ! 
+  integer :: i,j, number, seed, x, a,m,c
+  integer, dimension(:), allocatable :: sequance
+  integer :: flag = 0
+  
+  !error variable
+  integer :: err
   !     supply initial values of some variables:
   !     seed:   to start; a: 
   !     number: how many numbers we want to generate   
   print*,'Use LCM: x_(i+1)=mod(a*x_i+c,m)'
   print*,'Insert seed (=x_0), a, m, c >'
-  read (*,*) seed, a, m, c
-
-  min = m !inizialising the minimum number at the top of the sequance.
+  read (*,*) seed, a, m, c  
 
   print*,' How many numbers do you want to generate ?'
   read(*,*)number
+
+  !inizialising the serching tool for find the period
+  allocate(sequance(number), stat=err)
+  if (err /= 0) print *, "sequance: Allocation request denied"
+  sequance=0
   !
   OPEN(unit=1, file="random.dat", status="replace", action="write")
-  old = seed
-  !
-  do i = 1, number
-     x = mod ((a*old+c), m)
-     if ( x > max ) max = x
-     if ( x < min ) min = x
-     if ( x==5 ) print*, i
+  sequance(1) = seed
+
+  do i = 2, number , 1
+     x = mod ((a*sequance(i-1)+c), m)
+     sequance(i) = x
      WRITE (unit=1,fmt=*) i, x
-     old = x
   end do
-  close(1)
-  print*, min,max
+
+  print*, "------"
+
+  do i = 1, number, 1
+    do j = i+1, number, 1
+      if ( sequance(i) == sequance(j) ) then
+        print*, 'periodo trovato'
+        print*, abs(i-j)
+        flag = 1
+        exit
+      end if
+    end do
+    if ( flag == 1 ) exit
+  end do
+
+
+
+  !print conclusion
+  print*, minval(sequance),maxval(sequance)
   print*,' data saved in random.dat'
+  
+  !deallocation stuff
+  if (allocated(sequance)) deallocate(sequance, stat=err)
+  if (err /= 0) print *, "sequance: Deallocation request denied"
+  close(1)
+
   stop
 end program random_lcm
