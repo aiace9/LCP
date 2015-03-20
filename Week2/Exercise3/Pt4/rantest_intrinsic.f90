cd79d7a8295990c0c87b73e69ee0409838b5d043
Aiace9 <ruggero90@gmail.com>
2015-03-20 09:21:45 +0100

    improvement on week 2, exercise 4

--- Week2/Exercise3/Pt4/rantest_intrinsic.f90
+++ Week2/Exercise3/Pt4/rantest_intrinsic.f90
@@ -6,38 +6,72 @@ program rantest_intrinsic
 !
   implicit none
   real :: rnd
-  real, dimension (:), allocatable :: x
-  integer :: L,i,n_min,n_max,ran_int
+  integer, dimension (:), allocatable :: x
+  integer :: L,i,j,n_min,n_max
+  integer :: err,ios
 
-  ! generates ONE random number in [0,1[
-     call random_number(rnd)
-     print *,' A real random number in [0,1[ is:',rnd
+  logical :: flag = .false.
+  logical :: debug = .false.
 
   ! generates L random numbers in [0,1[
-     print*,' How many random numbers do you want to generate in [0,1[ ?'
-     print*,' Insert the length of the sequence >'
-     read(*,*)L        ! length of sequence
-    do i = 1,L
-     call random_number(rnd)
-     print *,rnd
-  end do
+  print*,' Insert the length of the sequence >'
+  read(*,*)L        ! length of sequence
 
   !  generates integer random numbers between n_min and n_max
-     print*,' Generate ',L,' integer random numbers in [n_min,n_max[ ;'
-     print*,' insert n_min, n_max >'
-     read(*,*),n_min,n_max
-    do i = 1,L
-     call random_number(rnd)
-     ran_int = (n_max - n_min + 1)*rnd + n_min
-     print *,ran_int
+  print*,' insert n_min, n_max >'
+  read(*,*)n_min,n_max
+
+  
+  if ( debug ) print*, 'debug1'
+  
+  allocate(x(L), stat=err)
+  if (err /= 0) print *, "x: Allocation request denied"
+  
+  if ( debug ) print*, 'debug2'
+  
+  open(unit=2, file="data.dat", iostat=ios, status="replace", action="write")
+  if ( ios /= 0 ) stop "Error opening file data.dat"
+  
+  if ( debug ) print*, 'debug3' 
+  
+  do i = 1, L, 1
+    call random_number(rnd)
+    x(i) = (n_max - n_min + 1)*rnd + n_min
+    if ( debug ) print*, 'debug = ', x(i)
   end do
 
-  !  use array x to generate and store L random numbers with a unique call
-  print*,' Generate other ',L,' real random numbers in [0,1[:'
-  allocate(x(L))
-  call random_number(x)
-  print*, x
-  deallocate(x)
+  if ( debug ) print*, 'debug4'
+
+  do i = 1, L, 1
+    if ( debug ) print*, 'debug = ', i
+    write(unit=2, fmt=*, iostat=ios) i, x(i)
+    if ( ios /= 0 ) stop "Write error in file unit 1" 
+  end do
+
+  if ( debug ) print*, 'done, starting period'
+
+  !period searcing
+  do i = 1, L, 1
+    do j = i+1, L, 1
+      if ( x(i) == x(j) ) then
+        print*, 'periodo trovato start:', i,' end:' , j
+        print*, abs(i-j)
+        flag = .true.
+        exit
+      end if
+    end do
+    if ( flag ) exit
+  end do
+  if (flag .eqv. .false.) print*, 'periodo non trovato'
+
+
+  close(unit=2, iostat=ios)
+  if ( ios /= 0 ) stop "Error closing file unit 1"
+  
+  if ( debug ) print*, 'done'
+
+  deallocate(x, stat=err)
+  if (err /= 0) print *, "x: Deallocation request denied"
 
 end program rantest_intrinsic
 
