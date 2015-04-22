cc199f2a5786aa42aa91fd23de93fc3d6fa94a5a
Aiace9 <ruggero90@gmail.com>
2015-04-22 12:15:09 +0200

    completed the istogram module, now it works

--- Week6/Exercise2/istogram_module.f90
+++ Week6/Exercise2/istogram_module.f90
@@ -9,16 +9,14 @@ module istogram_module
 
 	!privat declaretions
 	integer, parameter :: dp = selected_real_kind(14,200)
-	integer, dimension(:), allocatable :: hist
-	save
-	real(kind = dp) :: a, b, step
-	save
-	integer :: nbin
-	save
+	
+	integer, save, dimension(:), allocatable :: hist
+	real(kind = dp), save :: a, b, step
+	integer,save :: nbin
 
 
 contains
-	subroutine init(min,max,delta)
+	subroutine init_ist(min,max,delta)
 		real(kind = dp), intent(in) :: min,max,delta
 		integer :: err
 
@@ -26,33 +24,39 @@ contains
 	 	a = min
 		step = delta
 		nbin= int((b-a)/step)
-		allocate(hist(bin), stat=err)
+		allocate(hist(nbin), stat=err)
 		if (err /= 0) print *, "hist: Allocation request denied"
 		hist = 0		
-	end subroutine init
+	end subroutine init_ist
 
 	subroutine push_ist(x)
 		real(kind = dp), intent(in) :: x
 		integer :: ibin
+		logical :: debug = .false.
 
 		ibin = floor ( x / step) + 1
 
 		if (ibin <= nbin )then
       		hist(ibin) = hist(ibin) + 1
 	    end if
+
+	    if ( debug ) print*, 'D: hist(ibin)', hist(ibin)
 	end subroutine push_ist
 
-	subroutine save_data()
+	subroutine save_data_ist()
 		integer :: myunit, ibin, points
 		integer :: ios, err
+		logical :: debug = .false.
 
 		open(newunit=myunit, file='ist.dat', iostat=ios, status="unknown", action="write")
 		if ( ios /= 0 ) stop "Error opening file ist.dat"
 
-		points = !calculete the total points
+		points =  sum(hist, dim=1)!calculete the total points
+		if ( debug ) print*, 'D: number of points', points
+
 		
 		do ibin= 1 ,nbin
-    		write(unit=1,fmt=*)(ibin-0.5)*step - min ,hist(ibin)/float(points)/step
+    		write(unit=myunit,fmt=*)(ibin-0.5)*step - a ,hist(ibin)/float(points)/step
   		end do
 
 		
@@ -60,7 +64,7 @@ contains
 		if (err /= 0) print *, "hist: Deallocation request denied"
 
 		close(unit=myunit, iostat=ios)
-		if ( ios /= 0 ) stop "Error closing file unit", myunit
-	end subroutine save_data
+		if ( ios /= 0 ) stop "Error closing file ist.dat"
+	end subroutine save_data_ist
 
 end module istogram_module
---
+++ Week6/Exercise2/istogram_module_test.f90
@@ -0,0 +1,75 @@
+program istogram_module_test
+	 !where is indicated "valid only in this case" is because
+	 !there is hidden theorical calculation behind a value that is done for THIS distribution
+	use	istogram_module
+	
+	implicit none
+	integer, parameter :: dp = selected_real_kind(14,200)
+
+	integer :: N, points
+	integer :: i
+	real(kind = dp), dimension(:), allocatable :: rnd
+	real(kind = dp) :: x, med, med2, sigma2, z4, z2, z
+
+	!error flags
+	integer :: err
+
+	!histogram
+	call init_ist(0d0,5d2,1d0)
+	
+	!input
+	N=500
+	points =1000
+
+	!internal initialization
+	med = 0
+	med2 = 0
+	sigma2 = 0
+
+	allocate(rnd(N), stat=err)
+	if (err /= 0) print *, "rnd: Allocation request denied"
+
+	!points generation
+	do i = 1, points, 1
+		call random_number(rnd)
+		x = sum(rnd, dim=1)
+
+		call push_ist(x)
+
+	    !numerical check
+	    med = med + x
+	    med2 = med2 + x**2
+	    z = (x - N *0.5) / (N * 1/12.0) !valid only in this case
+	    z4 = z4 + z**4
+	    z2 = z2 + z**2
+	    end do
+
+	!histogram writinig
+	call save_data_ist()
+
+  	!more numerical stuff
+
+  	med = med/points
+  	med2 = med2/points
+  	z2= ( z2 / N )**2
+  	z4= ( z4 / N )
+  	sigma2 = med2 - med**2
+
+  	print*, 'final check'
+  	print*, '---'
+  	print*, 'numerical mean value = ', med
+  	print*, 'analitical mean value= ', N * 0.5 !valid only in this case
+  	print*, '---'
+  	print*, 'numerical sigma2 = ', sigma2
+  	print*, 'analitical sigma2= ', N * 1/12.0!valid only in this case
+  	print*, '---'
+  	print*, 'those number must be similar for some theorical reasons'
+  	print*, z4
+  	print*, 3 * z2
+  	
+	
+	if (allocated(rnd)) deallocate(rnd, stat=err)
+	if (err /= 0) print *, "rnd: Deallocation request denied"
+	
+
+end program istogram_module_test
